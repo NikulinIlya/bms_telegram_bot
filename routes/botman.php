fdacaf1ec61bc0bbbd3768da22a483946104a1cc
Ilya <ilia.nikulin@gmail.com>
2018-08-09 16:06:31 +0300

    telegram driver

--- routes/botman.php
+++ routes/botman.php
@@ -12,3 +12,4 @@ $botman->hears('Start conversation', BotManController::class.'@startConversation
 $botman->hears('/b {breed}', 'App\Http\Controllers\AllBreedsController@byBreed');
 $botman->hears('/s {breed}:{subBreed}', 'App\Http\Controllers\SubBreedController@random');
 $botman->hears('Start conversation', 'App\Http\Controllers\ConversationController@index');
+$botman->fallback('App\Http\Controllers\FallbackController@index');
